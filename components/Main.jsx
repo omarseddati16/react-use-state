3c56d86210bf9d3a47d710c586a2c6ae61bc218e
omarseddati16 <omarseddati16@gmail.com>
2025-08-28 15:59:42 +0200

    aggiunta hooks

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
 const Main = ({ languages }) => {
+  const [language, setlanguage] = useState(0)
   return (
     <main className="mt-4">
       <div className="container">
-        <div className="d-flex flex-wrap gap-2 col-6">
-          {languages.map((lang) => (
-            <li className="list-unstyled p-1" key={lang.id}>
-              <button className="btn btn-primary mt-2 ">{lang.title}</button>
-            </li>
-          ))}
-          <div className="card shadow-sm col-12 mt-4">
+        <div className="row">
+          <ul className="d-flex gap-2 col-12">
+            {languages.map((lang) => (
+              <li className="list-unstyled p-1" key={lang.id}>
+                <button className="btn btn-primary mt-2 "
+                  onClick={() => setlanguage(lang)}
+                >
+                  {lang.title}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className=" card shadow-sm col-6 mt-4">
             <div className="card-body">
-              <h5 class="card-title">HTML</h5>
-              <p class="card-text">HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
+              <h5 className="card-title">HTML</h5>
+              <p className="card-text">HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
             </div>
           </div>
         </div>
