ee80d9a33eeca2bb19cfe428ca14db0fc34569b8
omarseddati16 <omarseddati16@gmail.com>
2025-08-28 16:12:37 +0200

    fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const Main = ({ languages }) => {
-  const [language, setlanguage] = useState(0)
+  const [language, setlanguage] = useState[0];
   return (
     <main className="mt-4">
       <div className="container">
         <div className="row">
           <ul className="d-flex gap-2 col-12">
-            {languages.map((lang) => (
-              <li className="list-unstyled p-1" key={lang.id}>
+            {languages.map((newLanguages) => (
+              <li className="list-unstyled p-1" key={newLanguages.id}>
                 <button className="btn btn-primary mt-2 "
-                  onClick={() => setlanguage(lang)}
+                  onClick={() => setlanguage(newLanguages)}
                 >
-                  {lang.title}
+                  {newLanguages.title}
                 </button>
               </li>
             ))}
           </ul>
           <div className=" card shadow-sm col-6 mt-4">
             <div className="card-body">
-              <h5 className="card-title">HTML</h5>
-              <p className="card-text">HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.</p>
+              <h5 className="card-title">{language.title}</h5>
+              <p className="card-text">{language.description}</p>
             </div>
           </div>
         </div>
